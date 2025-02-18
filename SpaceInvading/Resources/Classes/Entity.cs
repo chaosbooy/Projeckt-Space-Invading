81d648e6bbdf778d464463041f5f9b14881adadc
chaosbooy <lpmelon234@gmail.com>
2025-02-18 17:34:53 +0100

    Adding basic classes
    I added some pages and classes for the start just to set things a bit up for the future

---
+++ SpaceInvading/Resources/Classes/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvading.Resources.Classes
+{
+    class Entity
+    {
+        public String Name;
+        public int Health;
+        public String[] IdleSprite;
+        public String[] MovingSprite;
+        public String[] DestroyedSprite;
+        
+
+
+        public Entity() { }
+
+
+
+        public void Death()
+        {
+
+        } 
+    }
+}
---
+++ SpaceInvading/Resources/Classes/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvading.Resources.Classes
+{
+    class Player : Entity
+    {
+        List<Item> Drops;
+        List<Item> Upgrades
+
+
+        public Player()
+        {
+
+        }
+    }
+}
